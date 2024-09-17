d79f6d423bb6816fa53993356783b81e1435dfcb
Katelyn Curtiss <153852200+KatelynCurtiss@users.noreply.github.com>
2024-09-17 10:37:35 -0400

    Create strings_methods.py

---
+++ strings_methods.py
@@ -0,0 +1,14 @@
+#Katelyn & Keily 
+# Setember 17 2024
+
+code = "the number of characters in this string. "
+
+capitalize = code.capitalize()
+
+print (capitalize)
+
+writing = "apples, apples,apples"
+
+count = writing.count("apples")
+
+print(count)
